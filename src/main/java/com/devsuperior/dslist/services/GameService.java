4f55cd1850a98b27029ce8e9c1d93dceda9b2f77
curso_básico_Dio <marrimacelso@gmail.com>
2024-11-17 00:52:03 -0300

    Game, Repository, Service, Controller

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dslist.dto.GameMiniDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repository.GameRepository;
+
+@Service
+public class GameService {
+	@Autowired
+	private GameRepository gameRepository;
+	public List<GameMiniDTO> findAll(){
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMiniDTO(x)).toList();
+	}
+}
